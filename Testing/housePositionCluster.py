19dd6cdc9783864962fecccc931f304a8e697514
bennettbijumathew <tekkit25637@gmail.com>
2024-09-28 15:57:40 +1000

    upgrades

--- Testing/housePositionCluster.py
+++ Testing/housePositionCluster.py
@@ -8,18 +8,44 @@ df = pd.read_csv("Datasets/melb_data (snapshot).csv")
 
 df.dropna(inplace=True)
 
-X = df.loc[:, ["Lattitude", "Longtitude"]]
+# X = df.loc[:, ["Lattitude", "Longtitude"]]
 
-model = KMeans(n_clusters=5, random_state=5)
+# model = KMeans(n_clusters=5, random_state=5)
 
-model.fit(X)
+# model.fit(X)
 
-all_predictions = model.predict(X)
+# all_predictions = model.predict(X)
+
+# plt.figure(figsize=(12, 6))
+# plt.scatter(X["Lattitude"], X["Longtitude"], c=all_predictions, cmap='viridis')
+# plt.title('Cluster of houses?')
+# plt.xlabel('Lattitude')
+# plt.ylabel('Longtitude')
+# plt.colorbar(label='Cluster')
+# plt.show()
+
+school_df = pd.read_csv('./Datasets/victoria-schools-2018.csv')
+# df = df[(df['Lattitude'] in range(-38.2 to -37.4) )]
+# df = df[(df["Rooms"] == roomAmount) & (df["Bathroom"] == bathroomAmount)]
+
+school_df = school_df[(school_df['Y'] >= -38.2) & (school_df['Y'] <= -37.4) & (school_df['X'] >= 144) & (school_df['X'] <= 145.6)] 
+
+X = df['Longtitude']
+Y = df['Lattitude']
+
+schools_X = school_df["X"]
+schools_Y = school_df["Y"]
 
 plt.figure(figsize=(12, 6))
-plt.scatter(X["Lattitude"], X["Longtitude"], c=all_predictions, cmap='viridis')
+plt.scatter(X, Y, color='blue')
+plt.scatter(schools_X, schools_Y, color='pink')
 plt.title('Cluster of houses?')
-plt.xlabel('Lattitude')
-plt.ylabel('Longtitude')
+plt.xlabel('Longtitude')
+plt.ylabel('Lattitude')
 plt.colorbar(label='Cluster')
 plt.show()
+
+
+
+# latitude in range of -38.2 to -37.4
+# longitude in range of 144 to 145.6
--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import pandas as pd
-import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error, r2_score
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.linear_model import LinearRegression
+from sklearn.cluster import KMeans
+from sklearn.cluster import DBSCAN
 from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans
+import numpy as np
 
 # Reads data from both CSV file and puts it into variables.
 first_dataset = pd.read_csv('./Datasets/melb_data (snapshot).csv')
@@ -28,9 +30,12 @@ df['Date'] =  pd.to_datetime(df['Date'], format='%d/%m/%Y')
 df["Postcode"] = df["Postcode"].astype('category')
 
 # drop columns where alot of data is missing TODO: Might have to change this code for the training? 
-df = df.drop(['Bedroom2', 'Landsize', 'BuildingArea', 'YearBuilt'], axis=1)
+df.drop(['Bedroom2', 'BuildingArea', 'YearBuilt'], axis=1, inplace = True)
 
-# Removes rows where there is values. 
+# Removes houses with landsizes outside of the range of 0 - 10000  
+df.drop(df[(df['Landsize'] < 0) | (df['Landsize'] > 10000)].index, inplace = True)
+
+# Removes rows where there is no values in the columns. 
 df.dropna(inplace=True)
 
 # Removes outliers where prices were less than $100,000 or more than $7,000,000.
@@ -48,13 +53,13 @@ plt.rcParams['axes.spines.top'] = False
 plt.rcParams['axes.spines.right'] = False
 plt.rcParams["font.family"] = "monospace"
 
-# Creates a distribution graph of number of houses vs house prices. 
+# Creates a distribution graph of number of houses vs average house prices. 
 def distrubution_house_prices(): 
-	sns.displot(df['Price'], color ='b')
-	plt.axvline(x = df['Price'].mean(), color='r', linestyle='--', linewidth=2)
+	sns.displot(df['Price'], color ='blue')
+	plt.axvline(x = df['Price'].mean(), color='red', linestyle='--', linewidth=2)
 	plt.xlabel('House Prices')
 	plt.ylabel('Number of Houses')
-	plt.title('Distrubution of Houses Prices')
+	plt.title('Distrubution of Average Houses Prices')
 	plt.show()
 
 # Creates a new bar plot showing the distribution of houses across regions. 
@@ -92,11 +97,26 @@ def scatter_distance_price():
 
 # Creates a scatter plot that shows the house prices based on the average room amount.
 def scatter_room_price():
-	plt.figure(figsize=(14, 12))
-	plt.scatter(df['Rooms'], df['Price'], alpha=0.5, color='b')
+	X = df[['Price']]
+	Y = df['Rooms']
+
+	# Splits the data so that 80% of the data is used to train the model, while the other 20% is used to test the model 
+	X_train, X_test, Y_train, Y_test = train_test_split(X, Y, train_size=0.8, test_size=0.2, random_state=0)
+
+	# Create a linear regression model that creates a fit that best represents the data using the trained data. 
+	model = LinearRegression()
+	model.fit(X_train, Y_train)
+
+	# The future trajectory of the data is then prdeicted using the tested variables. 
+	Y_pred = model.predict(X_test)
+
+	plt.figure(figsize=(12, 10))
+	plt.scatter(X_test, Y_test, alpha=0.5, color='blue')
+	plt.plot(X_test, Y_pred, color='red', linewidth=3, label='Predicted values')
+
 	plt.title('Scatter Plot of Average Rooms vs. House Price')
-	plt.xlabel('Average Number of Rooms per Household')
-	plt.ylabel('House Price (in $100,000)')
+	plt.xlabel('House Price (in $1,000,000)')
+	plt.ylabel('Number of Rooms per Household')
 	plt.grid(True)
 	plt.show()
 
@@ -114,9 +134,39 @@ def correlation_data():
 	plt.title('Correlation Matrix Heat Map')
 	plt.show()
 
+# Creates a scatter plot, that clusters groups of houses that have similar landsizes.
+def scatter_landsize_location(): 
+	X = df.loc[:, ["Landsize"]]
+
+	# Creates a KMeans model that groups data. 
+	model = KMeans(n_clusters=10, random_state=10)
+	model.fit(X)
+
+	# Clusters the houses into 8 groups based on the land size of the house. 
+	X["Cluster"] = model.predict(X)
+
+	# Creates a scatter plot that shows the location of houses, along with a color that shows the grouping of each house. 
+	plt.figure(figsize=(10, 6))
+	plt.scatter(df['Longtitude'], df['Lattitude'], c=X["Cluster"], cmap='viridis', edgecolors='white', linewidth=0.3)
+	plt.title('Scatter Plot of Houses, Grouped by Land Sizes')
+	plt.xlabel('Longtitude')
+	plt.ylabel('Lattitude')
+	plt.colorbar(label='Color Groups of Land Size')
+
+	# Creates a scatter plot that shows the clusters along with the landsize. 
+	plt.figure(figsize=(8, 6))
+	plt.scatter(X['Landsize'], X['Cluster'], c=X["Cluster"], cmap='viridis', edgecolors='white', linewidth=0.3, s=100)
+	plt.title('Scatter Plot of Landsize to Group of Houses')
+	plt.xlabel('Land Size')
+	plt.ylabel('Clusters')
+	plt.grid(True)
+
+	# Shows both of the graphs.
+	plt.show()
 
 # distrubution_house_prices()
 # bar_house_region()
 # scatter_distance_price()
 # scatter_room_price()
 # correlation_data()
+# scatter_landsize_location()
