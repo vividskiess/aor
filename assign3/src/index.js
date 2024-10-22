24e74f64b16f687b0921cfb973f29b4fa0cccb58
vividskies <dannyc52521@gmail.com>
2024-10-22 15:27:51 +1100

    updates

--- assign3/src/index.js
+++ assign3/src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { ThemeProvider, createTheme } from '@mui/material/styles';// Import MUI's ThemeProvider and createTheme for applying a theme
+import CssBaseline from '@mui/material/CssBaseline'; // CssBaseline for consistent baseline styles across browsers
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from 'react-router-dom'; 
+
+const theme = createTheme();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+   {/* Wrap the app in ThemeProvider and pass the theme for consistent styling */}
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Router>  {/* Wrap the App component in Router */}
+      <App />
+    </Router>
+    </ThemeProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
