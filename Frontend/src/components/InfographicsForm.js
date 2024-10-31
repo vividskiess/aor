1f5866876a2e66e03279af8f78010147e6bb596b
bennettbijumathew <tekkit25637@gmail.com>
2024-11-01 00:12:33 +1100

    updates - Added a Landsize Chart

--- Frontend/src/components/InfographicsForm.js
+++ Frontend/src/components/InfographicsForm.js
@@ -1,30 +1,54 @@
 import React, {useState} from 'react';
-import { TextField, Button, Container, Stack, Box } from '@mui/material';
+import { TextField, Button, Container, Stack } from '@mui/material';
 import BedroomChart from '../components/charts/BedroomChart'
 import axios from 'axios';
+import LandsizeChart from './charts/LandsizeChart';
 
 export default function InfographicsForm() {
 	const [postcode, setPostcode] = useState('')
-  const [bedrooms] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-	const [prices, setPrices] = useState([]);
+
+	const [rooms] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+	const [roomsPrices, setRoomsPrices] = useState([]);
+
+	// TODO: Implement once jack's code has been implemented.
+	// const [landsizes] = useState([100, 200, 300, 400, 500, 600, 700, 800, 900])
+	// const [landsizePrices, setLandsizePrices] = useState([]);
 
 	async function handleSubmit(event) {
 		event.preventDefault();
-    setPrices([])
+		setRoomsPrices([])
 
-    bedrooms.map(async (room) => {
-      await axios.get(`http://127.0.0.1:8000/Infographics/Bedroom/${postcode}/${room}`)
-        .then((response) => {
-          setPrices(prevPrices => [...prevPrices || [], response.data.price])
-        })
-        .catch((error) => {
-          throw Error("Error encountered: ", error);
-        })
-    })
+		// TODO: Implement once jack's code has been implemented.
+		// setLandsizePrices([])
+
+		rooms.map(async (room) => {
+			await axios.get(`http://127.0.0.1:8000/Infographics/Bedroom/${postcode}/${room}`)
+			.then((response) => {
+				setRoomsPrices(prevPrices => [...prevPrices || [], response.data.price])
+			})
+
+			.catch((error) => {
+				throw Error("Error encountered: ", error);
+			})
+		})
+
+		// TODO: Implement once jack's code has been implemented.
+		// landsizes.map(async (landsize) => {
+		// 	await axios.get(`http://127.0.0.1:8000/Infographics/Bedroom/${postcode}/${landsize}`)
+		// 	.then((response) => {
+		// 		setLandsizePrices(prevPrices => [...prevPrices || [], response.data.price])
+		// 	})
+
+		// 	.catch((error) => {
+		// 		throw Error("Error encountered: ", error);
+		// 	})
+		// })
 	}
+
 	return (
 		<Container disableGutters sx={{ p: 5 }}>
 			<h2>Predict prices of houses in your chosen suburb</h2>
+
 			<form onSubmit={handleSubmit}>
 				<Stack spacing={2} direction="row" sx={{marginBottom: 4}}>
 					<TextField
@@ -32,7 +56,7 @@ export default function InfographicsForm() {
 						variant='outlined'
 						color='secondary'
 						label="Postcode"
-            pattern="[0-9]*"
+            			pattern="[0-9]*"
 						onChange={e => setPostcode(e.target.value)}
 						value={postcode}
 						fullWidth
@@ -41,7 +65,13 @@ export default function InfographicsForm() {
 				</Stack>
 				<Button variant="outlined" color="secondary" type="submit">Submit</Button>
 			</form>
-      <BedroomChart prices={prices} bedrooms={bedrooms}/>
+
+			<BedroomChart prices={roomsPrices} bedrooms={rooms}/>
+
+			{
+				// TODO: Implement once jack's code has been implemented.
+				/* <LandsizeChart prices={landsizePrices} landsizes={landsizes}/> */
+			}
 		</Container>
 	)
 }
--- Frontend/src/components/charts/LandsizeChart.js
+++ Frontend/src/components/charts/LandsizeChart.js
@@ -1,36 +1,7 @@
-import axios from 'axios';
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Plot from 'react-plotly.js';
 
-export default function LandsizeChart() {
-	const [landsizes] = useState([100, 200, 300, 400, 500, 600, 700, 800, 900])
-	const [prices, setPrices] = useState([])
-	
-	// TODO: The code below is test code. Need to implement inputs for the code.
-	const suburb = 3754
-
-	// Fetches the price of the house
-	useEffect(() => {
-		const fetchPrices = async () => {
-			try {
-				// Maps over each room and fetches the predicted price for each room 
-				const newPrices = await Promise.all(landsizes.map(async (room) => {
-                    // TODO: Change the link.
-					const response = await axios.get(`http://127.0.0.1:8000/Infographics/Bedroom/${suburb}/${room}`);
-					return response.data.price;
-                }));
-
-				// Updates the state with the array of the predicted prices
-				setPrices(newPrices);
-			} 
-			// TODO: The code below is placeholder code. Need to implement proper error checking.
-			catch (error) {
-				console.error("Error fetching prices:", error);
-			}
-		};
-			fetchPrices()
-    }, [landsizes]); 
-
+export default function LandsizeChart({prices, landsizes}) {
 	return (
 		<Plot
 			data={[
@@ -45,16 +16,16 @@ export default function LandsizeChart() {
 					}
 				}
 			]}
-			
 			layout = {{
 				title: {
-					text:'Prediction of House Prices based on Bedrooms and Suburb',
+					text:'Predicted Prices of Houses in Postcode subrub baseed on landsize',
+					// text:'Prediction of House Prices based on Landsize',
 				}, 
 
 				xaxis: {
-					dtick: 1,
+					// dtick: 1,
 					title: {
-						text:'Land Sizes'
+						text:'Landsize'
 					}, 
 				},
 
